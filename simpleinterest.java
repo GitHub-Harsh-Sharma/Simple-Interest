be01fd3d38d81c5d19d9b35437ddc4d26a140390
Harsh Sharma <117378407+GitHub-Harsh-Sharma@users.noreply.github.com>
2022-12-17 15:02:06 +0530

    Add files via upload

---
+++ simpleinterest.java
@@ -0,0 +1,12 @@
+class simpleinterest
+{
+    public static void main(String args[])
+    {
+        int p,r,t,si;
+        p=13000;
+        r=12;
+        t=2;
+        si=p*r*t/100;
+        System.out.print("The simple interest is:"+si);
+    }
+}
